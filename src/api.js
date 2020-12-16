aec3008aec407d3bcee92cd275693f9952f78421
sourabhsaini11 <ss1841620@gmail.com>
2020-12-16 19:56:26 +0530

    api_url

---
+++ src/api.js
@@ -0,0 +1,34 @@
+const base_url = "https://api.rawg.io/api/";
+
+const getCurrentMonth = () => {
+    const month =new Date().getMonth() + 1;
+    if (month < 10) {
+        return `0${month}`;
+    } else {
+        return `${month}`;
+    }
+};
+
+const getCurrentDay = () => {
+    const day =new Date().getDate();
+    if (day < 10) {
+        return `0${day}`;
+    } else {
+        return `${day}`;
+    }
+};
+
+const currentYear =  new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popular_gamesURL = () => `${base_url}${popular_games}`;
+
+console.log(popular_gamesURL());
+
+
