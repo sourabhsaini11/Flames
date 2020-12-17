114aa4a6154d73de05a59e22cfdc6445bbae236e
sourabhsaini11 <ss1841620@gmail.com>
2020-12-17 19:13:34 +0530

    cards

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <h1>sadsa</h1>
       <Home />
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,103 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+//Redux
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+//Components
+import Game from "../components/Game";
+//Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
-
-function Home() {
-
+const Home = () => {
+  //FETCH GAMES
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  }); 
-
+  }, [dispatch]);
+  //Get that data back
+  const { popular, newGames, upComing } = useSelector((state) => state.games);
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <GameList>
+      <h2>upcoming Games</h2>
+      <Games>
+        {upComing.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
-}
+};
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { loadGames } from ".. /actions/gamesAction";
+
+
+// function Home() {
+
+//   const dispatch = useDispatch();
+//   useEffect(() => {
+//     dispatch(loadGames());
+//   }); 
+
+//   return (
+//     <div>
+//       <h1>Home</h1>
+//     </div>
+//   );
+// }
+
+// export default Home;
