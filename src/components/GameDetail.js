7e1a3af98b0e6551605516842a5312d4ab7fb3ea
sourabhsaini11 <ss1841620@gmail.com>
2020-12-26 14:59:40 +0530

    adding-icons

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,6 +6,12 @@ import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 //import { smallImage } from "../util";
+import playstation from "../img/playstation.svg";
+import steam from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
+import nintendo from "../img/nintendo.svg";
+import apple from "../img/apple.svg";
+import gamepad from "../img/gamepad.svg";
 
 
 const GameDetail = ({pathId}) => {
@@ -19,6 +25,23 @@ const GameDetail = ({pathId}) => {
       history.push('/');
     }
   }
+
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case "PlayStation 4":
+        return playstation;
+      case "Xbox One":
+        return xbox;
+      case "PC":
+        return steam;
+      case "Nintendo Switch":
+        return nintendo;
+      case "iOS":
+        return apple;
+      default:
+        return gamepad;
+    }
+  };
 
   //Data
   const { screen, game, isLoading } = useSelector((state) => state.detail);
@@ -36,7 +59,7 @@ const GameDetail = ({pathId}) => {
             <h3>Platforms</h3>
             <Platforms>
               {game.platforms.map((data) => (
-                <h3 key={data.platform.id}>{data.platform.name}</h3>
+                <img key={data.platform.id} src={getPlatform(data.platform.name)}  alt={data.platform.name} ></img>
               ))}
             </Platforms>
           </Info>
@@ -67,6 +90,7 @@ const CardShadow = styled(motion.div)`
   position: fixed;
   top: 0;
   left: 0;
+  z-index:5;
   &::-webkit-scrollbar {
     width: 0.5rem;
   }
@@ -88,6 +112,7 @@ const Detail = styled(motion.div)`
   position: absolute;
   left: 10%;
   color: black;
+  z-index:10;
   img {
     width: 100%;
   }
